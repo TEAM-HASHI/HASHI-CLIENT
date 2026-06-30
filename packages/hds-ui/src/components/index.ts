1fcbae0fb2375a1e1d5fc0ad04e75457ea04e7c7
chungyoung kim <107472367+chungyo@users.noreply.github.com>
2026-06-30 16:04:07 +0900

    Feat(hds-ui): HASHI-51 Bottom Navigation 구현 (#25)
    * feat(packages/hds-icons): HASHI-51 내비게이션 아이콘 색상 제어 지원
    
    * feat(packages/hds-ui): HASHI-51 BottomNavigation 컴포넌트 추가

--- packages/hds-ui/src/components/index.ts
+++ packages/hds-ui/src/components/index.ts
@@ -1,1 +1,7 @@
 export { Button } from './button/Button'
+
+export { BottomNavigation } from './bottomNavigation'
+export type {
+  BottomNavigationItem,
+  BottomNavigationProps,
+} from './bottomNavigation'
