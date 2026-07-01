6f74f2ce480313847dabf5c1aeee2431b70d0767
chungyoung kim <107472367+chungyo@users.noreply.github.com>
2026-07-01 10:02:22 +0900

    Feat(hds-ui): HASHI-52 Dialog 컴포넌트 구현 (#29)
    * feat(packages/hds-ui): HASHI-52 Dialog 컴포넌트 추가
    
    * style(packages/hds-ui): HASHI-52 Dialog 리뷰 반영
    
    * docs(packages/hds-ui): HASHI-52 Dialog 구현 기준 보강

--- packages/hds-ui/src/components/index.ts
+++ packages/hds-ui/src/components/index.ts
@@ -7,3 +7,18 @@ export type {
 } from './bottomNavigation'
 export { Tabs } from './tabs'
 export type { TabsItem, TabsProps } from './tabs'
+
+export { Dialog } from './dialog'
+export type {
+  DialogBodyProps,
+  DialogCloseProps,
+  DialogContentProps,
+  DialogDescriptionProps,
+  DialogFooterProps,
+  DialogHeaderProps,
+  DialogIconProps,
+  DialogRootProps,
+  DialogTitleProps,
+  DialogTriggerProps,
+  DialogType,
+} from './dialog'
