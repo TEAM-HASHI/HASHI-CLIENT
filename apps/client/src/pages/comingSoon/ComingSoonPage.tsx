a00b0d1ce6bd741ac554f4139262032749e8b5d5
jyeon03 <bjlyeon2003@naver.com>
2026-07-03 02:24:44 +0900

    feat(client): HASHI-66 ComingSoon 페이지 퍼블리싱

--- apps/client/src/pages/comingSoon/ComingSoonPage.tsx
+++ apps/client/src/pages/comingSoon/ComingSoonPage.tsx
@@ -1,3 +1,37 @@
+import { Button } from '@hashi/hds-ui'
+import { useNavigate } from 'react-router-dom'
+import { ROUTES } from '@/app/router/path'
+import emptyImage from './assets/empty.webp'
+
 export const ComingSoonPage = () => {
-  return <h1>준비중 페이지</h1>
+  const navigate = useNavigate()
+
+  return (
+    <main className="app-mobile-bottom-nav-content flex flex-col items-center justify-center text-center">
+      <img
+        alt="서비스 준비 중"
+        aria-hidden="true"
+        className="mb-[22.5px] h-auto w-33.75"
+        src={emptyImage}
+      />
+      <div>
+        <h1 className="typo-header-3 text-cool-gray-900 mb-2">
+          서비스를 준비하고 있어요.
+        </h1>
+        <p className="typo-body-5 text-cool-gray-500 mb-5 leading-[1.4]">
+          더 편한 Hashi 이용을 위해
+          <br />
+          현재 기능을 준비하고 있어요.
+        </p>
+        <Button
+          className="typo-sub-header-2 w-59.25"
+          onClick={() => navigate(ROUTES.home)}
+          size="md"
+          type="button"
+        >
+          홈으로 돌아가기
+        </Button>
+      </div>
+    </main>
+  )
 }
