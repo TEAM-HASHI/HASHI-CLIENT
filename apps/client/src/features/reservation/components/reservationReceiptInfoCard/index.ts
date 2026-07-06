009d3334309b6cf01343163e65c3b68016efd764
jyeon03 <bjlyeon2003@naver.com>
2026-07-06 14:22:28 +0900

    feat(client): HASHI-71 예약 접수 정보 카드 구현

---
+++ apps/client/src/features/reservation/components/reservationReceiptInfoCard/index.ts
@@ -0,0 +1,5 @@
+export { ReservationReceiptInfoCard } from './ReservationReceiptInfoCard'
+export type {
+  ReservationReceiptInfoCardProps,
+  ReservationReceiptInfoItem,
+} from './ReservationReceiptInfoCard'
