ab993148578450d15b1a53924505ffe63b545181
jyeon03 <bjlyeon2003@naver.com>
2026-07-06 17:54:41 +0900

    fix(client): HASHI-70 로그인 바텀시트 전역 노출 제거

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,22 +1,7 @@
-import { useState } from 'react'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from '@/app/router'
-import { AuthGateBottomSheet } from '@/features/auth/components/authGateBottomSheet'
 
 export const App = () => {
-  const [isAuthGateOpen, setIsAuthGateOpen] = useState(true)
-
-  return (
-    <>
-      <RouterProvider router={router} />
-      <AuthGateBottomSheet
-        open={isAuthGateOpen}
-        onKakaoPress={() => {
-          // TODO: 카카오 OAuth 플로우 연결
-        }}
-        onOpenChange={setIsAuthGateOpen}
-      />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
