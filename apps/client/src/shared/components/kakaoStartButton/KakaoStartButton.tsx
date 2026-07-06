decf045852f5b3533cf89338fdd0702ed7e1c380
백지연 <147830921+jyeon03@users.noreply.github.com>
2026-07-06 17:58:33 +0900

    Feat(client): HASHI-70 LoginRequired 페이지 퍼블리싱 (#52)
    * feat(hds-icons): HASHI-70 KakaoIcon 추가
    
    * refactor(client): HASHI-70 KakaoStartButton shared로 이동
    
    * feat(client): HASHI-70 로그인 필요 페이지 구현
    
    * fix(hds-ui): HASHI-70 BottomNavigation 선택 상태 optional 처리
    
    * chore(client): HASHI-70 임시 인증 상태 기본값 변경
    
    * fix(client): HASHI-70 LoginRequiredPage main 중첩 제거
    
    * feat(client): HASHI-70 KakaoStartButton className 확장
    
    * fix(client): HASHI-70 로그인 바텀시트 전역 노출 제거

--- apps/client/src/shared/components/kakaoStartButton/KakaoStartButton.tsx
+++ apps/client/src/shared/components/kakaoStartButton/KakaoStartButton.tsx
@@ -1,21 +1,22 @@
+import { KakaoIcon } from '@hashi/hds-icons'
 import { Button } from '@hashi/hds-ui'
-
-import { KakaoIcon } from '@/features/auth/assets/KakaoIcon'
 
 type KakaoStartButtonProps = {
   children: string
   onPress: () => void
+  className?: string
 }
 
 export const KakaoStartButton = ({
   children,
   onPress,
+  className,
 }: KakaoStartButtonProps) => {
   return (
     <Button
-      className="typo-sub-header-1 bg-point-200 relative h-[58px] rounded-full text-black"
+      className={`typo-sub-header-1 bg-point-200 relative h-14.5 rounded-full text-black ${className}`}
       leftIcon={
-        <KakaoIcon className="absolute top-1/2 left-[19px] size-6 -translate-y-1/2" />
+        <KakaoIcon className="absolute top-1/2 left-4.75 size-6 -translate-y-1/2" />
       }
       onClick={onPress}
       width="full"
---
+++ apps/client/src/shared/components/kakaoStartButton/index.ts
@@ -0,0 +1,1 @@
+export { KakaoStartButton } from './KakaoStartButton'
